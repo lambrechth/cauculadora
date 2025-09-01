d672b0bec457c6b064856d4271a4f8ae95055d37
queixo minuscolo <lambrecht.daniel@escola.pr.gov.br>
2025-09-01 08:02:13 -0300

    A

--- aaaaa/script.js
+++ aaaaa/script.js
@@ -4,11 +4,23 @@ function imc(){
     const nome = document.getElementById('nome').value;
     const altura = document.getElementById('altura').value;
     const peso = document.getElementById('peso').value;
-    const resultado = document.getElementById('resultado');
+    const resultado = document.getElementById
+    ('resultado');
 
-    const valorIMC = (peso/altura*altura).toFixed
+    
 
-    resultado.textContent = `${nome}seu IMC é ${valorIMC}!`;
+    if(home !="" && altura !="" && peso !=""){
+
+    const valorIMC = (peso/(altura*altura)).toFixed(1);
+    let classicacao = "";
+
+    if(valorIMC > 18.5){
+        classicacao = "abaixo do peso"
+    }
+
+    }
+
+    resultado.textContent = `${nome} seu IMC é ${valorIMC}!`;
 }
 
 calcular.addEventListener('click',imc);
